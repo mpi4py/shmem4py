df43dd4cad7a8d5014b76df94056b99cc6c9236e
Marcin Rogowski <marcin.rogowski@kaust.edu.sa>
2023-06-28 13:28:01 +0300

    test oo

--- demo/lock_oo.py
+++ demo/lock_oo.py
@@ -1,14 +1,18 @@
 # adapted from http://openshmem.org/site/sites/default/site_files/OpenSHMEM-1.5.pdf (Example 45)
 
+import numpy as np
 from shmem4py import shmem
 
 lock = shmem.Lock()
 mype = shmem.my_pe()
 
+val = np.array([0], dtype='i')
 count = shmem.array([0], dtype='i')
-val = shmem.array([0], dtype='i')
+shmem.barrier_all()
+print(f"{mype}: past the barrier")
 
 lock.acquire()
+print(f"{mype}: set the lock")
 shmem.get(val, count, 0)
 print(f"{mype}: count is {val[0]}")
 val[0] += 1
