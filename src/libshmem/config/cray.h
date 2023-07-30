f153f305f16b46e5bf9e09478d3e596893ca82d1
Jeff Hammond <jeff.science@gmail.com>
2023-07-30 09:53:05 +0300

    Support for Cray OpenSHMEM-X 11

--- src/libshmem/config/cray.h
+++ src/libshmem/config/cray.h
@@ -1,4 +1,27 @@
 #ifndef PySHMEM_CONFIG_CRAY_H
 #define PySHMEM_CONFIG_CRAY_H
 
+#if CRAY_SHMEM_MAJOR_VERSION >= 11
+#define PySHMEM_HAVE_shmem_malloc_with_hints 1
+#define PySHMEM_HAVE_shmem_team_t 1
+#define PySHMEM_HAVE_SHMEM_CTX_INVALID 1
+#define PySHMEM_HAVE_shmem_amo_nbi 1
+#define PySHMEM_HAVE_shmem_put_signal 1
+#define PySHMEM_HAVE_shmem_signal_fetch 1
+#define PySHMEM_HAVE_shmem_signal_wait_until 1
+#define PySHMEM_HAVE_shmem_broadcast 1
+#define PySHMEM_HAVE_shmem_collect 1
+#define PySHMEM_HAVE_shmem_fcollect 1
+#define PySHMEM_HAVE_shmem_alltoall 1
+#define PySHMEM_HAVE_shmem_alltoalls 1
+#define PySHMEM_HAVE_shmem_broadcastmem 1
+#define PySHMEM_HAVE_shmem_collectmem 1
+#define PySHMEM_HAVE_shmem_fcollectmem 1
+#define PySHMEM_HAVE_shmem_alltoallmem 1
+#define PySHMEM_HAVE_shmem_alltoallsmem 1
+#define PySHMEM_HAVE_shmem_reduce 1
+#define PySHMEM_HAVE_shmem_wait_test_many 1
+/* #define PySHMEM_HAVE_shmem_pcontrol 1 */
+#endif
+
 #endif
